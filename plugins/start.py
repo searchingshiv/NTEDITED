2b6e45cea827c8914fcdbe619a7f812b964cc545
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-27 13:50:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -68,28 +68,51 @@ async def delete_after_delay(message, delay):
     await message.delete()
 
     for msg in messages:
+        if bool(CUSTOM_CAPTION) and bool(msg.document):
+            caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html,
+                                            filename=msg.document.file_name)
+        else:
+            caption = "" if not msg.caption else msg.caption.html
 
-            if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
-            else:
-                caption = "" if not msg.caption else msg.caption.html
+        if DISABLE_CHANNEL_BUTTON:
+            reply_markup = msg.reply_markup
+        else:
+            reply_markup = None
 
-            if DISABLE_CHANNEL_BUTTON:
-                reply_markup = msg.reply_markup
-            else:
-                reply_markup = None
+        try:
+            lodu = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML,
+                                  reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+            asyncio.ensure_future(delete_after_delay(lodu, 5))  # Schedule deletion after 5 seconds
 
-            try:
-                lodu = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                asyncio.ensure_future(delete_after_delay(lodu, 5))  # Schedule deletion after 5 seconds
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML,
+                           reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+        except:
+            pass
 
-            
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-            except:
-                pass
-    return
+    else:
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("😊 About Me", callback_data="about"),
+                    InlineKeyboardButton("🔒 Close", callback_data="close")
+                ]
+            ]
+        )
+        await message.reply_text(
+            text=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
+            ),
+            reply_markup=reply_markup,
+            disable_web_page_preview=True,
+            quote=True
+        )
+        return
 
     
 #=====================================================================================##
