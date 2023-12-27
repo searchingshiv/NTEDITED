577ff0ed418b51e864d35eec225075f2aae4756b
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-27 12:06:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -76,8 +76,13 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                lodu = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
+
+        # Add a 5-second delay before deleting the message
+                await asyncio.sleep(5)
+                await lodu.delete()
+            
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
