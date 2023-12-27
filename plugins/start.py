8cc4709844e79a1f6f528aba80fe44983a737ded
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-27 13:42:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,28 +90,6 @@ async def delete_after_delay(message, delay):
             except:
                 pass
     return
-    else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
-        return
 
     
 #=====================================================================================##
