7b91b8c50b9c26fa60315de6b9d8ef0f3b8b8667
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-27 12:56:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -89,7 +89,7 @@ async def delete_after_delay(message, delay):
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        return
+    return
     else:
         reply_markup = InlineKeyboardMarkup(
             [
