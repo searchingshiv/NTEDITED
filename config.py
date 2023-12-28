5ad13759147d6476ef8f16e9ed04c5c885de617f
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-28 15:53:47 +0530

    config.py update kr lena
    config.py update kr lena

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6769527523:AAHKqB_OHZiObBdc1rC4XfqMY6Rgc2Fm8WI")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "apna add kro")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "14136462"))
