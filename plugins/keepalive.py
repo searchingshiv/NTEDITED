2e177917f29b1490aa6f1b52adbdf24e61eabffb
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-28 13:40:52 +0530

    Update keepalive.py

--- plugins/keepalive.py
+++ plugins/keepalive.py
@@ -6,8 +6,7 @@ from WebStreamer import Var
 logger = logging.getLogger("keep_alive")
 
 async def ping_server():
-    sleep_time = 60
-
+    sleep_time = Var.PING_INTERVAL
     logger.info("Started with {}s interval between pings".format(sleep_time))
     while True:
         await asyncio.sleep(sleep_time)
@@ -15,7 +14,7 @@ async def ping_server():
             async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(total=10)
             ) as session:
-                async with session.get(www.teamsilent.in) as resp:
+                async with session.get(Var.URL) as resp:
                     logger.info("Pinged server with response: {}".format(resp.status))
         except TimeoutError:
             logger.warning("Couldn't connect to the site URL..")
