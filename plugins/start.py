f157e64d482a3a7e77114c8afbba06c38a20c62e
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2023-12-28 11:21:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,7 +4,7 @@
 
 
 import os
-import asyncio import sleep, create_task
+import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
