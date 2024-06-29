75bc275b11d134a1d89adbec370f008bfd5122f9
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2024-06-29 13:13:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT=8081
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
 class Bot(Client):
     def __init__(self):
